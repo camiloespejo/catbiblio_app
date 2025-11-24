e17f8ceb872505bf1a0a931a05c0ddd6280a36dc
Luis Casas <zs21013875@estudiantes.uv.mx>
2025-11-24 13:43:21 -0600

    fix: refactor QueryParams to use ChangeNotifier for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:catbiblio_app/l10n/app_localizations.dart';
-import 'package:catbiblio_app/models/controllers_data.dart';
 import 'package:catbiblio_app/models/finder_params.dart';
 import 'package:catbiblio_app/models/global_provider.dart';
 import 'package:catbiblio_app/models/query_params.dart';
@@ -7,7 +6,6 @@ import 'package:catbiblio_app/ui/views/book_view.dart';
 import 'package:catbiblio_app/ui/views/finder_view.dart';
 import 'package:catbiblio_app/ui/views/home_view.dart';
 import 'package:catbiblio_app/ui/views/marc_view.dart';
-import 'package:catbiblio_app/ui/views/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
@@ -17,12 +15,16 @@ import 'url_strategy_nonweb.dart'
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   /// Set URL strategy based on platform, if not set will crash on mobile
   configureAppUrlStrategy();
   await dotenv.load();
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => GlobalProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => GlobalProvider()),
+        ChangeNotifierProvider(create: (_) => QueryParams()),
+      ],
       child: const MainApp(),
     ),
   );
@@ -49,6 +51,7 @@ class _MainAppState extends State<MainApp> {
   @override
   void initState() {
     super.initState();
+
     /// Define app routes for web
     _router = GoRouter(
       initialLocation: '/',
@@ -57,31 +60,6 @@ class _MainAppState extends State<MainApp> {
           path: '/',
           builder: (BuildContext context, GoRouterState state) {
             return HomeView(onLocaleChange: (locale) => setLocale(locale));
-          },
-        ),
-        GoRoute(
-          path: '/search',
-          builder: (BuildContext context, GoRouterState state) {
-            String itemtype = state.uri.queryParameters['itemtype'] ?? '';
-            String libraryid = state.uri.queryParameters['libraryid'] ?? '';
-            String filter = state.uri.queryParameters['filter'] ?? '';
-            String query = state.uri.queryParameters['query'] ?? '';
-
-            ControllersData controllersData = ControllersData(
-              libraryEntries: [],
-              itemTypeEntries: [],
-              filterEntries: [],
-            );
-
-            return SearchView(
-              controllersData: controllersData,
-              queryParams: QueryParams(
-                library: libraryid,
-                searchBy: filter,
-                searchQuery: query,
-                itemType: itemtype,
-              ),
-            );
           },
         ),
         GoRoute(
